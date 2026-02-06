f23e4f46520e8f7ba282854224ff8c7a03cd6268
Jesse Caldwell <CollectionOfAtoms@gmail.com>
2026-02-05 22:33:42 -0800

    Remove deadspace in tracks.

--- src/data/tracks.js
+++ src/data/tracks.js
@@ -12,11 +12,11 @@ export const tracks = [
   {
     title: 'Frozen In',
     description: 'The first piano piece I have ever written. It\'s simple, but I like it',
-    src: '/music/Frozen_in.mpeg',
+    src: '/music/Frozen_in_cut.mp3',
   },
   {
     title: 'Transatlanticism',
     description: 'Solo piano performance.  Death Cab For Cutie Cover',
-    src: '/music/Transatlanticism.mp3',
+    src: '/music/Transatlanticism-cut.mp3',
   },
 ];
